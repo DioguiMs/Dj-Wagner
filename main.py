578b1df2ae6e7a6276d204b1c7663450f7082bde
DioguiMs <diogomsilva148@gmail.com>
2024-10-28 11:35:33 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord, os, yt_dlp, asyncio
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
+
+
+if __name__ == '__main__':
+    pass
